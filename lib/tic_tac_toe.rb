08cc971baf68edb5e50594b3eed344c8d39bb86a
Michael Kim <m.kim4247@gmail.com>
2018-09-26 04:20:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,13 +60,13 @@ class TicTacToe
   #   return counter 
   # end
   
-  def current_player(board)
-    if turn_count(board) % 2 == 0
-      return "X"
-    else 
-      return "O"
-    end 
-  end 
+  # def current_player(board)
+  #   if turn_count(board) % 2 == 0
+  #     return "X"
+  #   else 
+  #     return "O"
+  #   end 
+  # end 
 
 
 end 
