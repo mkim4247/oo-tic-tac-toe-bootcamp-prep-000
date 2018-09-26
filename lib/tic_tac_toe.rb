a83fd93a202f1a00f8b60ddb7a852c852e756c7b
Michael Kim <m.kim4247@gmail.com>
2018-09-26 04:36:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -107,16 +107,16 @@ class TicTacToe
     end 
   end
   
-  # def play(board)
-  #   while !over?(board)
-  #     turn(board)
-  #   end
-  #   if won?(board)
-  #     puts "Congratulations #{winner(board)}!"
-  #   elsif draw?(board)
-  #     puts "Cat's Game!"
-  #   end
-  # end
+  def play(board)
+    while !over?(board)
+      turn(board)
+    end
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
+  end
 
 
 
