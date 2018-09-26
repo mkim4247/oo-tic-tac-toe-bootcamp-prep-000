b7829e688b704c38964fdad3852fe9af146e1093
Michael Kim <m.kim4247@gmail.com>
2018-09-26 04:18:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,13 @@ class TicTacToe
     @board[index] = token 
   end
 
-
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil 
+      return false 
+    else
+      return true 
+    end 
+  end
 
 
 
