70a0b763eed478171b9612b52bb7580b8c95eb74
Michael Kim <m.kim4247@gmail.com>
2018-09-26 04:12:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,9 +14,17 @@ class TicTacToe
     [2, 5, 8]
   ]
 
-
-
-
+  def display_board(@board)
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(user)
+    user.to_i - 1
+  end
 
 
 
